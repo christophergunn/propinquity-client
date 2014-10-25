6341d45793bbb33999e7cc57bfc336c149d979c4
Anthony Fielding <antfie@gmail.com>
2014-10-25 19:59:25 +0100

    Code changes for running on iPhone

--- propinquity/www/js/controllers.js
+++ propinquity/www/js/controllers.js
@@ -6,39 +6,19 @@ angular.module('starter.controllers', [])
     var token = 'T1==cGFydG5lcl9pZD00NTA0MzY2MiZzaWc9MzcwYzA0Mzg3NjQyZjBhODZmNTI5YzVkZjk0ODgxYWE3NzE4ZDQyZTpzZXNzaW9uX2lkPTFfTVg0ME5UQTBNelkyTW41LU1UUXhOREkwTURRNU1Ua3lOWDVZUlM5T2NYbG9OME5HV1c4d05tcHlPVmxRV0cweVdFSi1VSDQmY3JlYXRlX3RpbWU9MTQxNDI1MTk5MSZub25jZT04OTg3ODMmcm9sZT1QVUJMSVNIRVI=';
 
 	// Initialize an OpenTok Session object
-	var session = TB.initSession(sessionId);
+	var session = TB.initSession(apiKey, sessionId);
 	var uuid = getUuid();
 
-	// Initialize a Publisher, and place it into the element with id="publisher"
-	var publisher = TB.initPublisher(apiKey, 'publisher');
-
-	$scope.messages = []
+	$scope.messages = [];
 
 	// Attach event handlers
 	session.on({
-	    // This function runs when session.connect() asynchronously completes
-	    sessionConnected: function (event) {
-	        // Publish the publisher we initialzed earlier (this will trigger 'streamCreated' on other
-	        // clients)
-	        session.publish(publisher);
-	    },
 
-	    // This function runs when another client publishes a stream (eg. session.publish())
-	    streamCreated: function (event) {
-	        // Create a container for a new Subscriber, assign it an id using the streamId, put it inside
-	        // the element with id="subscribers"
-	        var subContainer = document.createElement('div');
-	        // subContainer.id = 'stream-' + event.stream.streamId;
-	        // document.getElementById('subscribers').appendChild(subContainer);
-
-	        // Subscribe to the stream that caused this event, put it inside the container we just made
-	        session.subscribe(event.stream, subContainer);
-	    },
 
 	    signal: function(event) {
-	    	console.log(event.data)
-	    	var parts = event.data.split('|')
-	    	$scope.messages.push({ name:parts[0] + ' (' + moment(parts[2]).format('h:mm:ssa')+ ')', message:parts[1]})
+	    	console.log(event.data);
+	    	var parts = event.data.split('|');
+	    	$scope.messages.push({ name:parts[0] + ' (' + moment(parts[2]).format('h:mm:ssa')+ ')', message:parts[1]});
 
 			$ionicScrollDelegate.scrollBottom();
 	    }
@@ -46,25 +26,25 @@ angular.module('starter.controllers', [])
 
     $scope.newMessage = {
         text: ""
-    }
+    };
 
     $scope.sendMessage = function() {
     	session.signal({
     		data: 'vince|' + $scope.newMessage.text + '|' + new Date()
     	}, function(error) {
 		    if (error) {
-		      console.log("signal error ("
+		      alert("signal error ("
 		                   + error.code
 		                   + "): " + error.reason);
 		    } else {
 		      console.log("signal sent.");
 		    }
-    	})
-    }
+    	});
+    };
 
 	$(function() {
 		var response = geoFindMe(function(lat, lon) {
-		    $.post("http://http://propinquity-server.azurewebsites.net/client/gps", { id: uuid, lat: lat, lon: lon });
+		    $.post("http://propinquity-server.azurewebsites.net/client/gps", { id: uuid, lat: lat, lon: lon });
 		});
 	});
 
@@ -78,7 +58,7 @@ angular.module('starter.controllers', [])
     var token = 'T1==cGFydG5lcl9pZD00NTA0MzY2MiZzaWc9MzcwYzA0Mzg3NjQyZjBhODZmNTI5YzVkZjk0ODgxYWE3NzE4ZDQyZTpzZXNzaW9uX2lkPTFfTVg0ME5UQTBNelkyTW41LU1UUXhOREkwTURRNU1Ua3lOWDVZUlM5T2NYbG9OME5HV1c4d05tcHlPVmxRV0cweVdFSi1VSDQmY3JlYXRlX3RpbWU9MTQxNDI1MTk5MSZub25jZT04OTg3ODMmcm9sZT1QVUJMSVNIRVI=';
 
 	// Initialize an OpenTok Session object
-	var session = TB.initSession(sessionId);
+	var session = TB.initSession(apiKey, sessionId);
 	var uuid = getUuid();
 
 	// Initialize a Publisher, and place it into the element with id="publisher"
@@ -109,7 +89,7 @@ angular.module('starter.controllers', [])
 
 	$(function() {
 		geoFindMe(function(lat, lon) {
-		    $.post("http://http://propinquity-server.azurewebsites.net/client/gps", { id: uuid, lat: lat, lon: lon });
+		    $.post("http://propinquity-server.azurewebsites.net/client/gps", { id: uuid, lat: lat, lon: lon });
 		});
 	});
 
