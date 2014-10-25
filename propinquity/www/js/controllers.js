f9f6bd8183b821b118e19ccce8e093159cae2582
Vince Lee <vincent.lee@drllimited.co.uk>
2014-10-25 17:21:08 +0100

    pulled in PoC

--- propinquity/www/js/controllers.js
+++ propinquity/www/js/controllers.js
@@ -1,22 +1,119 @@
 angular.module('starter.controllers', [])
 
 .controller('ChatCtrl', function($scope) {
-	    $scope.messages = [
+    var apiKey = '45043662';
+    var sessionId = '1_MX40NTA0MzY2Mn5-MTQxNDI0MDQ5MTkyNX5YRS9OcXloN0NGWW8wNmpyOVlQWG0yWEJ-UH4';
+    var token = 'T1==cGFydG5lcl9pZD00NTA0MzY2MiZzaWc9MzcwYzA0Mzg3NjQyZjBhODZmNTI5YzVkZjk0ODgxYWE3NzE4ZDQyZTpzZXNzaW9uX2lkPTFfTVg0ME5UQTBNelkyTW41LU1UUXhOREkwTURRNU1Ua3lOWDVZUlM5T2NYbG9OME5HV1c4d05tcHlPVmxRV0cweVdFSi1VSDQmY3JlYXRlX3RpbWU9MTQxNDI1MTk5MSZub25jZT04OTg3ODMmcm9sZT1QVUJMSVNIRVI=';
+
+	// Initialize an OpenTok Session object
+	var session = TB.initSession(sessionId);
+	var uuid = getUuid();
+
+	// Initialize a Publisher, and place it into the element with id="publisher"
+	var publisher = TB.initPublisher(apiKey, 'publisher');
+
+	$scope.messages = [
         { name:'goose', message:'hello'},
         { name:'mav', message:'goodbye'},
     ]
+
+	// Attach event handlers
+	session.on({
+	    // This function runs when session.connect() asynchronously completes
+	    sessionConnected: function (event) {
+	        // Publish the publisher we initialzed earlier (this will trigger 'streamCreated' on other
+	        // clients)
+	        session.publish(publisher);
+	    },
+
+	    // This function runs when another client publishes a stream (eg. session.publish())
+	    streamCreated: function (event) {
+	        // Create a container for a new Subscriber, assign it an id using the streamId, put it inside
+	        // the element with id="subscribers"
+	        var subContainer = document.createElement('div');
+	        subContainer.id = 'stream-' + event.stream.streamId;
+	        document.getElementById('subscribers').appendChild(subContainer);
+
+	        // Subscribe to the stream that caused this event, put it inside the container we just made
+	        session.subscribe(event.stream, subContainer);
+	    },
+
+	    signal: function(event) {
+	    	console.log(event)
+	    }
+	});
 
     $scope.newMessage = {
         text: ""
     }
 
     $scope.sendMessage = function() {
-        $scope.messages.push({ name: 'vincey', message: this.newMessage.text })
+    	session.signal({
+    		data: $scope.newMessage.text
+    	}, function(error) {
+		    if (error) {
+		      console.log("signal error ("
+		                   + error.code
+		                   + "): " + error.reason);
+		    } else {
+		      console.log("signal sent.");
+		    }
+    	})
     }
 
+	$(function() {
+		var response = geoFindMe(function(lat, lon) {
+		    $.post("http://http://propinquity-server.azurewebsites.net/client/gps", { id: uuid, lat: lat, lon: lon });
+		});
+	});
+
+	// Connect to the Session using the 'apiKey' of the application and a 'token' for permission
+	session.connect(apiKey, token);
 })
 
 .controller('VideoCtrl', function($scope) {
+    var apiKey = '45043662';
+    var sessionId = '1_MX40NTA0MzY2Mn5-MTQxNDI0MDQ5MTkyNX5YRS9OcXloN0NGWW8wNmpyOVlQWG0yWEJ-UH4';
+    var token = 'T1==cGFydG5lcl9pZD00NTA0MzY2MiZzaWc9MzcwYzA0Mzg3NjQyZjBhODZmNTI5YzVkZjk0ODgxYWE3NzE4ZDQyZTpzZXNzaW9uX2lkPTFfTVg0ME5UQTBNelkyTW41LU1UUXhOREkwTURRNU1Ua3lOWDVZUlM5T2NYbG9OME5HV1c4d05tcHlPVmxRV0cweVdFSi1VSDQmY3JlYXRlX3RpbWU9MTQxNDI1MTk5MSZub25jZT04OTg3ODMmcm9sZT1QVUJMSVNIRVI=';
+
+	// Initialize an OpenTok Session object
+	var session = TB.initSession(sessionId);
+	var uuid = getUuid();
+
+	// Initialize a Publisher, and place it into the element with id="publisher"
+	var publisher = TB.initPublisher(apiKey, 'publisher');
+
+	// Attach event handlers
+	session.on({
+
+	    // This function runs when session.connect() asynchronously completes
+	    sessionConnected: function (event) {
+	        // Publish the publisher we initialzed earlier (this will trigger 'streamCreated' on other
+	        // clients)
+	        session.publish(publisher);
+	    },
+
+	    // This function runs when another client publishes a stream (eg. session.publish())
+	    streamCreated: function (event) {
+	        // Create a container for a new Subscriber, assign it an id using the streamId, put it inside
+	        // the element with id="subscribers"
+	        var subContainer = document.createElement('div');
+	        subContainer.id = 'stream-' + event.stream.streamId;
+	        document.getElementById('subscribers').appendChild(subContainer);
+
+	        // Subscribe to the stream that caused this event, put it inside the container we just made
+	        session.subscribe(event.stream, subContainer);
+	    }
+	});
+
+	$(function() {
+		geoFindMe(function(lat, lon) {
+		    $.post("http://http://propinquity-server.azurewebsites.net/client/gps", { id: uuid, lat: lat, lon: lon });
+		});
+	});
+
+	// Connect to the Session using the 'apiKey' of the application and a 'token' for permission
+	session.connect(apiKey, token);
 })
 
 .controller('FriendsCtrl', function($scope, Friends) {
