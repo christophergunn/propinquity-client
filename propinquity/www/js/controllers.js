f15b8f6a50c366db7f68b40636004d493bed86d7
Vince Lee <vincent.lee@drllimited.co.uk>
2014-10-25 18:15:36 +0100

    chat over rtc

--- propinquity/www/js/controllers.js
+++ propinquity/www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('ChatCtrl', function($scope) {
+.controller('ChatCtrl', function($scope, $ionicScrollDelegate) {
     var apiKey = '45043662';
     var sessionId = '1_MX40NTA0MzY2Mn5-MTQxNDI0MDQ5MTkyNX5YRS9OcXloN0NGWW8wNmpyOVlQWG0yWEJ-UH4';
     var token = 'T1==cGFydG5lcl9pZD00NTA0MzY2MiZzaWc9MzcwYzA0Mzg3NjQyZjBhODZmNTI5YzVkZjk0ODgxYWE3NzE4ZDQyZTpzZXNzaW9uX2lkPTFfTVg0ME5UQTBNelkyTW41LU1UUXhOREkwTURRNU1Ua3lOWDVZUlM5T2NYbG9OME5HV1c4d05tcHlPVmxRV0cweVdFSi1VSDQmY3JlYXRlX3RpbWU9MTQxNDI1MTk5MSZub25jZT04OTg3ODMmcm9sZT1QVUJMSVNIRVI=';
@@ -12,10 +12,7 @@ angular.module('starter.controllers', [])
 	// Initialize a Publisher, and place it into the element with id="publisher"
 	var publisher = TB.initPublisher(apiKey, 'publisher');
 
-	$scope.messages = [
-        { name:'goose', message:'hello'},
-        { name:'mav', message:'goodbye'},
-    ]
+	$scope.messages = []
 
 	// Attach event handlers
 	session.on({
@@ -31,15 +28,19 @@ angular.module('starter.controllers', [])
 	        // Create a container for a new Subscriber, assign it an id using the streamId, put it inside
 	        // the element with id="subscribers"
 	        var subContainer = document.createElement('div');
-	        subContainer.id = 'stream-' + event.stream.streamId;
-	        document.getElementById('subscribers').appendChild(subContainer);
+	        // subContainer.id = 'stream-' + event.stream.streamId;
+	        // document.getElementById('subscribers').appendChild(subContainer);
 
 	        // Subscribe to the stream that caused this event, put it inside the container we just made
 	        session.subscribe(event.stream, subContainer);
 	    },
 
 	    signal: function(event) {
-	    	console.log(event)
+	    	console.log(event.data)
+	    	var parts = event.data.split('|')
+	    	$scope.messages.push({ name:parts[0] + ' (' + moment(parts[2]).format('h:mm:ssa')+ ')', message:parts[1]})
+
+			$ionicScrollDelegate.scrollBottom();
 	    }
 	});
 
@@ -49,7 +50,7 @@ angular.module('starter.controllers', [])
 
     $scope.sendMessage = function() {
     	session.signal({
-    		data: $scope.newMessage.text
+    		data: 'vince|' + $scope.newMessage.text + '|' + new Date()
     	}, function(error) {
 		    if (error) {
 		      console.log("signal error ("
